edd7f94ed2fff89380772fdc505f58f87f1e3a82
Faizan Fazal <39065186+ffazal@users.noreply.github.com>
2023-05-09 14:55:18 -0400

    displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
-    game: Game
+  game: Game;
 }
 
-const GameCard = ( { game }: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
